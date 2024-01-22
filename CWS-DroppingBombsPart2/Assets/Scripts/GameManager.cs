012af030a64bce4cf0912d5126df2487f3078efa
Koi-codeninjas <CamdenSharp123@gmail.com>
2024-01-22 17:39:11 -0500

    44
    44

---
+++ CWS-DroppingBombsPart2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+
+    void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner.active = false;
+        title.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            spawner.active = true;
+            title.SetActive(false);
+        }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12)
+            {
+                Destroy(bombObject);
+            }
+        }
+
+    }
+}
--- CWS-DroppingBombsPart2/Assets/Spawner.cs
+++ CWS-DroppingBombsPart2/Assets/Spawner.cs
@@ -9,16 +9,38 @@ public class Spawner : MonoBehaviour
     public bool active = false;
     public Vector2 delayRange = new Vector2(1, 2);
 
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        ResetDealy();
-        StartCoroutine(EnemyGenerator));
+        ResetDelay();
+        StartCoroutine(EnemyGenerator());
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2;
+        objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator EnemyGenerator()
     {
-        
+        yield return new WaitForSeconds(delay);
+        if (active)
+        {
+            float randomX = Random.Range(screenBounds.x - objectWidth, screenBounds.x * -1 + objectWidth);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+
+            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
+            ResetDelay();
+        }
+
+        StartCoroutine(EnemyGenerator());
     }
-}
+
+        void ResetDelay()
+        {
+            delay = Random.Range(delayRange.x, delayRange.y);
+        }
+    }
